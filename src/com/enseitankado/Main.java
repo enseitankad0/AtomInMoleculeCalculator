35e4b3346bd151d8a793fdd35ff23f5ae0a0ad29
enseitankad0 <94kamil94@gmail.com>
2018-06-13 21:06:14 +0200

    Readme

--- src/com/enseitankado/Main.java
+++ src/com/enseitankado/Main.java
@@ -1,44 +1,13 @@
 package com.enseitankado;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         ParseMolecule parseMolecule = new ParseMolecule();
 
 
+        System.out.println(parseMolecule.getAtoms("H2O"));
 
-
-
-//        parseMolecule.getAtoms("2H2O");
-//
-//        parseMolecule.getAtoms("22He3PO4");
-
-
-
-
-        //parseMolecule.getAtoms("2Ca2O");
-
-//parseMolecule.getAtoms("22K2SO4");
-//        parseMolecule.getAtoms("22H2O");
-
-
-//        String kamil = "Kamil";
-//        System.out.println(kamil.indexOf("a"));
-
-
-//        String water = "H2O";
-//        parseMolecule.getAtoms(water); // return [H: 2, O: 1]
-//
-//        String magnesiumHydroxide = "Mg(OH)2";
-//        parseMolecule.getAtoms(magnesiumHydroxide); // return ["Mg": 1, "O": 2, "H": 2]
-//
-//        String fremySalt = "K4[ON(SO3)2]2";
-//        parseMolecule.getAtoms(fremySalt); // return ["K": 4, "O": 14, "N": 2, "S": 4]
-//
-//        parseMolecule.getAtoms("pie"); // throw an IllegalArgumentException
 
     }
 }
